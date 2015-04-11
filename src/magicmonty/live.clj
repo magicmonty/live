ff7e43259f67532169f61223639c797b0b8c07b6
Martin Gondermann <magicmonty@pagansoft.de>
2015-04-12 00:51:56 +0200

    Added code for 'Fibonacci in A minor'

--- src/magicmonty/live.clj
+++ src/magicmonty/live.clj
@@ -1,6 +1,10 @@
-(ns magicmonty.live)
+(ns magicmonty.live
+  (:use [overtone.live :refer :all]
+        [leipzig.melody :refer :all]
+        [magicmonty.live.synths])
+  (require [leipzig.scale :as scale]
+           [leipzig.live :as ll]
+           [leipzig.chord :as chord]
+           [leipzig.temperament :as temperament]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
